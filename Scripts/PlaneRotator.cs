d5e6575accae8e1fe861fa1408ffd274d4797141
jesus <jesus.gutierrez.kipp@gmail.com>
2024-12-09 02:43:21 -0500

    tilt ball win the game

---
+++ Scripts/PlaneRotator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			//向上转
+			transform.Rotate(Vector3.right * Time.deltaTime * 30);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			//向下转
+			transform.Rotate(Vector3.left * Time.deltaTime * 30);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			//向左转
+			transform.Rotate(Vector3.forward * Time.deltaTime * 30);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			//向右转
+			transform.Rotate(Vector3.back * Time.deltaTime * 30);
+		}
+
+	}
+
+    private void FixedUpdate()
+    {
+		
+	}
+}
